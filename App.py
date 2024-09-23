0d03b331cd5d5063a65b5f5f4be78b697651ce49
Madhavi0512 <solletimadhavi1913@gmail.com>
2024-09-23 09:10:44 +0530

    Update App.py

--- App.py
+++ App.py
@@ -15,7 +15,7 @@ st.title("Credit Card Fraud Detection")
 
 @st.cache_data
 def load_data():
-    data = pd.read_csv(r"C:\Users\DELL\Downloads\creditcard.csv.zip")
+    data = pd.read_csv(r"https://drive.google.com/file/d/1JsdrNqQr5nQszcGQ7QqRYgFGllSESd_N/view?usp=drive_link")
     return data
 
 data = load_data()
